478c03082bf40b209428e45a42cc7553ab3f3535
wrf <470964201@qq.com>
2016-12-12 15:36:56 +0800

    染色工具

--- baselibrary/src/main/java/com/hzgktyt/vr/baselibrary/utils/DrawableTintUtils.java
+++ baselibrary/src/main/java/com/hzgktyt/vr/baselibrary/utils/DrawableTintUtils.java
@@ -1,8 +1,35 @@
 package com.hzgktyt.vr.baselibrary.utils;
 
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.StateListDrawable;
+import android.support.v4.graphics.drawable.DrawableCompat;
+
 /**
  * Created by wrf on 2016/12/12.
  */
 
 public class DrawableTintUtils {
+
+    public static void tintDrawable(Drawable drawable, int colorInt) {
+        drawable = DrawableCompat.wrap(drawable);
+        DrawableCompat.setTint(drawable.mutate(), colorInt);
+    }
+
+    public static Drawable getTintDrawable(Drawable drawable, int colorInt) {
+        drawable = DrawableCompat.wrap(drawable);
+        DrawableCompat.setTint(drawable.mutate(), colorInt);
+
+        return drawable;
+    }
+
+
+    public static StateListDrawable selectStateListDrawable(Drawable normaldrawable, Drawable selecteddrawable) {
+        StateListDrawable states = new StateListDrawable();
+        states.addState(new int[]{android.R.attr.state_selected},
+                selecteddrawable);
+        states.addState(new int[]{},
+                normaldrawable);
+
+        return states;
+    }
 }
