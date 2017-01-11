ebb065df48db4618e46d8a9de98ae7d526003e47
wrf <470964201@qq.com>
2017-01-11 11:39:56 +0800

    1.添加了视频列表item付费标识 2.添加带有footer的adapter 3.设置了播放页面的视频封面图

---
+++ vrvideo/src/androidTest/java/com/hngkyt/vr/activity/LoginActivityTest.java
@@ -0,0 +1,8 @@
+package com.hngkyt.vr.activity;
+
+/**
+ * Created by wrf on 2017/1/5.
+ */
+
+public class LoginActivityTest {
+}
---
+++ vrvideo/src/main/java/com/hngkyt/vr/view/FooterGridLayoutManager.java
@@ -0,0 +1,8 @@
+package com.hngkyt.vr.view;
+
+/**
+ * Created by wrf on 2017/1/11.
+ */
+
+public class FooterGridLayoutManager {
+}
